9bd2238a7aec519b6de1419b4c53fdc2405be5f9
Michelle <mich.fit315@gmail.com>
2026-03-04 14:18:09 -0500

    Fix migration dependency for job expiry email flag

--- board/migrations/0003_job_expiry_email_sent.py
+++ board/migrations/0003_job_expiry_email_sent.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('board', '0002_careerfitlead'),
+        ("board", "0001_initial"),
     ]
 
     operations = [
